26c21a9cbbd566bcfd1496bb8fb753033d098a4e
jolieyolie <congyi.h.f@gmail.com>
2025-05-06 22:06:16 +0200

    D: analyze Book Frankenstein

--- main.py
+++ main.py
@@ -8,15 +8,25 @@ def main(filepath):
 #main("books/frankenstein.txt")
 text_book = get_book_text("books/frankenstein.txt")
 
-
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
 from stats import get_num_words
 get_num_words(text_book)
-
+print("--------- Character Count -------")
 from stats import get_num_char
 char_counts = get_num_char(text_book)
 #print(f"Total 't' count: {char_counts.get('t', 0)}")
-for char, count in char_counts.items():
-    print(f"'{char}': {count}")
+#for char, count in char_counts.items():
+    #print(f"'{char}': {count}")
+
+from stats import list_book
+
+characters = list_book(char_counts)
+for char in characters:
+    if char[0].isalpha():
+        print(f'{char[0]}: {char[1]}')
+print("============= END ===============")
 #t_positions = []
 #for i, char in enumerate(text_book.lower()):
     #if char == 't':
--- stats.py
+++ stats.py
@@ -1,14 +1,12 @@
 def get_num_words(text_book):
     num_words = len(text_book.split())
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
 def get_num_char(text_book):
     text_lower = text_book.lower()  # Convert text to lowercase for uniform counting
     char_counts = {}   # Dictionary to hold character counts
-   
     # Debug print: Show a small part of the text to ensure it's correctly converted
     #print(f"First 100 characters (lowercase): {repr(text_lower[:100])}")
-   
     for char in text_lower:
         if char in char_counts:
             char_counts[char] += 1
@@ -16,3 +14,10 @@ def get_num_char(text_book):
             char_counts[char] = 1
     return char_counts
 
+def sort_on(item):
+    return item[1]
+
+def list_book(char_counts):
+    list_of_char =list(char_counts.items())
+    list_of_char.sort(reverse=True, key=sort_on)
+    return list_of_char
